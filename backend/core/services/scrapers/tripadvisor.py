65cd255255054fcdd02d99ee34ba9e7b70344ef3
MatiusDev <mateomonsalve502@gmail.com>
2025-08-27 00:11:28 -0500

    FIX: Resolve merge conflicts on branch

--- backend/core/services/scrapers/tripadvisor.py
+++ backend/core/services/scrapers/tripadvisor.py
@@ -13,7 +13,6 @@ from typing import Dict, List, Any
 
 from bs4 import BeautifulSoup
 
-<<<<<<< HEAD
 from .interface import ScrapingInterface
 
 from core.utils.constants.headers import HEADERS
@@ -26,40 +25,22 @@ class TripAdvisorScraper(ScrapingInterface):
     self.host = self.config["HOST"]
     self.url_base = self.config["URL_BASE"]
     self.selectors = self.config["SELECTORS"]
-=======
-from core.utils.constants.headers import USER_AGENT_HEADERS, FULL_SESSION_HEADERS
-from .interface import ScrapingInterface
-
-class TripAdvisorScraper(ScrapingInterface):
-  HOST = "https://www.tripadvisor.co"
-  URL_BASE = f"{HOST}/FindRestaurants?geo=297478&establishmentTypes=10591%2C11776%2C16556%2C9900%2C9901%2C9909&broadened=false"
->>>>>>> 0d7cae4 (NEW: Adding factory pattern on scraping services behavior)
 
   async def get_links(self, offset_range: int = 2) -> List[str]:
     restaurant_links = []
     offsets = [i * 30 for i in range(offset_range)]
 
-<<<<<<< HEAD
     self.session.headers.update(HEADERS["USER_AGENT_HEADERS"])
     self.session.headers.update({'Referer': self.url_base})
 
     print("--- Iniciando Scraping de TripAdvisor (Lista) ---")
     for offset in offsets:
       current_url = f"{self.url_base}&offset={offset}"
-=======
-    self.session.headers.update(USER_AGENT_HEADERS)
-    self.session.headers.update({'Referer': self.URL_BASE})
-
-    print("--- Iniciando Scraping de TripAdvisor (Lista) ---")
-    for offset in offsets:
-      current_url = f"{self.URL_BASE}&offset={offset}"
->>>>>>> 0d7cae4 (NEW: Adding factory pattern on scraping services behavior)
       try:
         response = await self.session.get(current_url)
         response.raise_for_status()
         soup = BeautifulSoup(response.content, 'html.parser')
         
-<<<<<<< HEAD
         main_container = soup.select_one(self.selectors["MAIN_CONTAINER"])
         if not main_container: continue
 
@@ -67,15 +48,6 @@ class TripAdvisorScraper(ScrapingInterface):
           url = link_tag.get('href')
           if url and url.startswith('/'):
             full_url = f"{self.host}{url}"
-=======
-        main_container = soup.find('div', attrs={'data-automation': 'LeftRailMain'})
-        if not main_container: continue
-
-        for link_tag in main_container.select('a[href*="Restaurant_Review"]'):
-          url = link_tag.get('href')
-          if url and url.startswith('/'):
-            full_url = f"{self.HOST}{url}"
->>>>>>> 0d7cae4 (NEW: Adding factory pattern on scraping services behavior)
             if full_url not in restaurant_links:
               restaurant_links.append(full_url)
                 
@@ -89,43 +61,26 @@ class TripAdvisorScraper(ScrapingInterface):
   async def get_links_detail(self, detail_url: str) -> Dict[str, Any]:
     print(f"Scraping página de detalle: {detail_url}")
     try:
-<<<<<<< HEAD
       self.session.headers.update(HEADERS["FULL_SESSION_HEADERS"])
       self.session.headers.update({'Referer': self.url_base})
-=======
-      self.session.headers.update(FULL_SESSION_HEADERS)
-      self.session.headers.update({'Referer': self.URL_BASE})
->>>>>>> 0d7cae4 (NEW: Adding factory pattern on scraping services behavior)
 
       response = await self.session.get(detail_url)
       response.raise_for_status()
       soup = BeautifulSoup(response.content, 'html.parser')
 
-<<<<<<< HEAD
       detail_container = soup.select_one(self.selectors["DETAIL_CONTAINER"])
-=======
-      detail_container = soup.find('div', attrs={'data-test-target': 'restaurants-detail'})
->>>>>>> 0d7cae4 (NEW: Adding factory pattern on scraping services behavior)
       if not detail_container:
         raise Exception(f"Contenedor de detalle no encontrado en {detail_url}")
 
       image_urls = []
-<<<<<<< HEAD
       image_presentation_div = detail_container.select_one(self.selectors["IMAGE_PRESENTATION_CONTAINER"])
       
       if image_presentation_div:
         source_tags = image_presentation_div.select(self.selectors["IMAGE_SOURCE"])
-=======
-      image_presentation_div = detail_container.find('div', attrs={'aria-label': 'Presentación de imágenes'})
-      
-      if image_presentation_div:
-        source_tags = image_presentation_div.select('div > div > button > div > picture > source')
->>>>>>> 0d7cae4 (NEW: Adding factory pattern on scraping services behavior)
         for source in source_tags:
           if source.has_attr('srcset'):
             img_url = source['srcset'].split(' ')[0]
             if img_url.startswith('/'):
-<<<<<<< HEAD
               img_url = f"{self.host}{img_url}"
             image_urls.append(img_url)
             
@@ -134,16 +89,6 @@ class TripAdvisorScraper(ScrapingInterface):
       menu_tag = detail_container.select_one(self.selectors["MENU_BUTTON"])
       phone_tag = detail_container.select_one(self.selectors["PHONE_LINK"])
       email_tag = detail_container.select_one(self.selectors["EMAIL_LINK"])
-=======
-              img_url = f"{self.HOST}{img_url}"
-            image_urls.append(img_url)
-            
-      link_urls = {}
-      website_tag = detail_container.find('a', attrs={'data-automation': 'restaurantsWebsiteButton'})
-      menu_tag = detail_container.find('a', attrs={'data-automation': 'restaurantsMenuButton'})
-      phone_tag = detail_container.find('a[href^="tel:"]')
-      email_tag = detail_container.find('a[href^="mailto:"]')
->>>>>>> 0d7cae4 (NEW: Adding factory pattern on scraping services behavior)
       
       link_urls['website'] = website_tag.get('href') if website_tag else None
       link_urls['menu'] = menu_tag.get('href') if menu_tag else None
@@ -161,10 +106,6 @@ class TripAdvisorScraper(ScrapingInterface):
       }
       
     except httpx.RequestError as e:
-<<<<<<< HEAD
-=======
-      # Es mejor lanzar una excepción específica que el controlador pueda manejar
->>>>>>> 0d7cae4 (NEW: Adding factory pattern on scraping services behavior)
       raise Exception(f"Error de scraping de detalle para {detail_url}: {str(e)}")
     finally:
       await asyncio.sleep(2)
