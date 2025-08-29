5e08308f57a224fc055a709ef3b4051cf55ef4fe
MatiusDev <mateomonsalve502@gmail.com>
2025-08-29 17:30:46 -0500

    FIX: Solving branch problems

--- backend/main.py
+++ backend/main.py
@@ -12,10 +12,6 @@ load_dotenv(dotenv_path=env_path)
 from core.router import routes as api_routes
 from core.database.connection import init_db
 
-from core.router import routes as api_routes
-
-from core.router import routes as api_routes
-
 app = FastAPI()
 
 @app.on_event("startup")
